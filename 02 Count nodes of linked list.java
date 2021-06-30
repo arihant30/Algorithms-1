24047c46543d4c8f963e2f7ec4bde403f8d876a8
arihant30 <78214117+arihant30@users.noreply.github.com>
2021-06-30 22:00:24 +0530

    Create 02 Count nodes of linked list.java

---
+++ 02 Count nodes of linked list.java
@@ -0,0 +1,15 @@
+class Solution
+{
+    //Function to count nodes of a linked list.
+    public static int getCount(Node head)
+    {
+        int n=0;
+        while(head!=null){
+            n++;
+            head=head.next;
+        }
+        return n;
+    
+    }
+}
+    
